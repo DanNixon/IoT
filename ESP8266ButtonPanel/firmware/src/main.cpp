d91459c282b9c8a2de5e212a1b51632369112312
Dan Nixon <dan@dan-nixon.com>
2016-08-31 17:34:32 +0100

    add oled

--- ESP8266ButtonPanel/firmware/src/main.cpp
+++ ESP8266ButtonPanel/firmware/src/main.cpp
@@ -1,14 +1,28 @@
+#include <Adafruit_SSD1306.h>
 #include <MCP23S17.h>
+
+Adafruit_SSD1306 g_oled;
 
 MCP g_inputA(1, 10);
 MCP g_inputB(2, 10);
 MCP g_outputA(3, 10);
 MCP g_outputB(4, 10);
 
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
 void setup()
 {
+  /* Initialise OLED display */
+  g_oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  g_oled.display();
+
+  /* Initialise port expanders (buttons) */
   g_inputA.begin();
   g_inputB.begin();
+
+  /* Initialise port expanders (LEDs) */
   g_outputA.begin();
   g_outputB.begin();
 }
