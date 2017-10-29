87cf72f72fa84189c0a4a447f9f751b8627ccedf
Dan Nixon <dan@dan-nixon.com>
2017-10-29 10:58:51 +0000

    Add sample config

---
+++ generic_neopixel_lighting_hass/src/_config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define PIXEL_COUNT 11
+#define PIXEL_PIN D7
+#define PIXEL_MODE NEO_BRG + NEO_KHZ800
+
+#define STATE_TOPIC "lights/garage/workbench"
+#define COMMAND_TOPIC "lights/garage/workbench/cmd"
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define MQTT_SERVER_IP ""
+#define MQTT_CLIENT_ID ""
+
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
